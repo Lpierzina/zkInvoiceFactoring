4b5adbef366b55fc2e7bcf65cdd3863011038635
Your Name <you@example.com>
2025-06-12 16:33:51 -0700

    Add portable nargo binary for Heroku deploy

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ const result = execSync(`${nargoPath} execute`, {
     }
 });
 
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Backend running on http://localhost:${PORT}`);
